75f6386fbbc4da754d60bd7a85cfe6a3a0c1e8f8
Nathan Greene <nathangreene3@gmail.com>
2019-08-14 23:45:07 -0400

    Hard restart

--- perceptron.go
+++ perceptron.go
@@ -1,138 +1,142 @@
-package main
+package perceptron
 
 import (
 	"fmt"
 	"log"
 	"math"
 	"math/rand"
-	"strings"
+
+	"github.com/nathangreene3/math/linalg/matrix"
+	"github.com/nathangreene3/math/linalg/vector"
 )
 
-// perceptron.go
-// Nathan Greene
-// Fall 2018
+// Decider returns a value on the range [0,1] given some input.
+type Decider func(float64) float64
 
-// perceptron is a set of weights and a bias.
-type perceptron struct {
-	// weights is an ordered set of real values
-	weights []float64
-	// bias is the default weight applied
-	bias float64
+// Perceptron is a set of weights and a bias.
+type Perceptron struct {
+	dimensions int
+	weights    vector.Vector
+	bias       float64
+	decider    Decider
 }
 
 // Stringer does something in using perceptron.String.
-var _ = fmt.Stringer(&perceptron{})
+var _ = fmt.Stringer(&Perceptron{})
 
-// String returns a formatted string representation of a perceptron. A
-// perceptron is represented as
-// 	[weights], [bias]: [0.0, ..., 0.0], 0.0.
-func (p *perceptron) String() string {
-	a := make([]string, 0, len(p.weights))
-	for i := range p.weights {
-		a = append(a, fmt.Sprintf("%0.2f", p.weights[i]))
-	}
-	return fmt.Sprintf("[%s], %0.2f", strings.Join(a, ", "), p.bias)
-}
-
-// newPerceptron initiates an empty perceptron with a specified number of
-// dimensions. All weights and the bias are set to zero.
-func newPerceptron(dimensions int) *perceptron {
+// New initiates an untrained perceptron of a specified number of dimensions.
+func New(dimensions int, decider Decider) *Perceptron {
 	// Initially, -1 < weights, bias < 1
-	p := &perceptron{
-		weights: make([]float64, 0, dimensions),
-		bias:    1 - 2*rand.Float64(),
+	return &Perceptron{
+		dimensions: dimensions,
+		weights:    vector.New(dimensions, func(i int) float64 { return 1 - 2*rand.Float64() }),
+		bias:       1 - 2*rand.Float64(),
+		decider:    decider,
 	}
-	for i := 0; i < dimensions; i++ {
-		p.weights = append(p.weights, 1-2*rand.Float64())
-	}
-	return p
 }
 
-// feedForward computes the perceptron decision (result) given an input
-// value. A decision function must return a value on the range [0,1].
-func (p *perceptron) feedForward(input []float64, decision func(float64) float64) float64 {
-	result := p.bias
-	for i := range input {
-		result += input[i] * p.weights[i]
-	}
-	return decision(result) // threshold, sigmoid, etc.
-}
-
-// backPropagate adjusts the weights by delta given for a given input.
-func (p *perceptron) backPropagate(input []float64, delta float64) {
+// BackPropagate adjusts the weights by delta given for a given input.
+func (p *Perceptron) BackPropagate(input vector.Vector, delta float64) {
+	deltaInput := input.Copy()
+	deltaInput.Multiply(delta)
+	p.weights.Add(deltaInput)
 	p.bias += delta
-	for i := range input {
-		p.weights[i] += delta * input[i]
-	}
 }
 
-// learn trains the perceptron given a set of training data (inputs), a
+// FeedForward computes the perceptron decision (result) given an input
+// value. A decision function must return a value on the range [0,1].
+func (p *Perceptron) FeedForward(input vector.Vector) float64 {
+	return p.decider(p.weights.Dot(input) + p.bias)
+}
+
+// String returns a formatted string representation of a perceptron.
+func (p *Perceptron) String() string {
+	return fmt.Sprintf("[%0.2f], %0.2f", p.weights, p.bias)
+}
+
+// Train trains the perceptron given a set of training data (inputs), a
 // function accepting training data (trainer), and the learning rate.
-func (p *perceptron) learn(decision func(float64) float64, inputs [][]float64, class []float64) {
-	maxCount := 1000
-	for p.verify(inputs, class, decision) < 0.9 {
-		for i := range inputs {
-			p.backPropagate(inputs[i], 0.01*(class[i]-p.feedForward(inputs[i], decision)))
+func (p *Perceptron) Train(inputs matrix.Matrix, class vector.Vector, rate, accuracy float64) {
+	var (
+		maxCount = 1000
+		n, _     = inputs.Dimensions()
+	)
+
+	for p.Verify(inputs, class) < accuracy {
+		for i := 0; i < n; i++ {
+			p.BackPropagate(inputs[i], rate*(class[i]-p.FeedForward(inputs[i])))
 		}
 
 		maxCount--
 		if maxCount == 0 {
 			fmt.Println(p.String())
-			log.Fatal("perceptron failed to learn")
+			log.Fatal("perceptron failed to train")
 		}
 	}
 }
 
-// verify returns the ratio of the number of correct classifications to
+// Verify returns the ratio of the number of correct classifications to
 // the total number of inputs to classify.
-func (p *perceptron) verify(inputs [][]float64, class []float64, decision func(float64) float64) float64 {
-	count := float64(len(inputs)) // Number of inputs
-	correct := count              // Number of correct results
-	for i := range inputs {
-		if p.feedForward(inputs[i], decision) != class[i] {
-			correct--
+func (p *Perceptron) Verify(inputs matrix.Matrix, class vector.Vector) float64 {
+	var (
+		correct float64
+		n, _    = inputs.Dimensions()
+	)
+
+	for i := 0; i < n; i++ {
+		if p.FeedForward(inputs[i]) == class[i] {
+			correct++
 		}
 	}
-	return correct / count
+
+	return correct / float64(n)
 }
 
 // setWeightsBias force-sets the weights and bias to specified values.
-func (p *perceptron) setWeightsBias(w []float64, b float64) {
-	if len(p.weights) != len(w) {
+func (p *Perceptron) setWeightsBias(w vector.Vector, b float64) {
+	n := len(p.weights)
+	if n != len(w) {
 		log.Fatal("weight dimension mismatch")
 	}
-	for i := range p.weights {
+
+	p.bias = b
+	for i := 0; i < n; i++ {
 		p.weights[i] = w[i]
 	}
-	p.bias = b
 }
 
 //-----------------------------------------------------------------------
-// Decision functions
+// Deciders
 // These functions return a value on the range [0,1] for any real x. They
 // are used to determine what the result of a perceptron should be.
 //-----------------------------------------------------------------------
 
-// threshold is a simple decision function alternative to the logistic
+// Threshold is a simple decision function alternative to the logistic
 // function (sigmoid) or other decision functions. It returns 1 if x is
 // positive and 0 otherwise.
-func threshold(x float64) float64 {
+func Threshold(x float64) float64 {
 	if 0 < x {
 		return 1
 	}
+
 	return 0
 }
 
-// sigmoid returns a value on the range (0,1) for any real x.
-func sigmoid(x float64) float64 {
-	return 1 / (1 + 1/math.Exp(x))
+// Sigmoid returns a value on the range (0,1) for any real x.
+func Sigmoid(x float64) float64 {
+	// Also called the logistic.
+
+	// f(x) = 1/(1 + exp(-x)) = exp(x)/(1 + exp(x)) = 1 - f(-x)
+	// See sigmoidDeriv for properties of f'.
+	y := math.Exp(x)
+	return y / (1 + y)
 }
 
-// sigmoidDeriv returns the derivative of the sigmoid function evaluated
+// SigmoidDeriv returns the derivative of the sigmoid function evaluated
 // at x.
-func sigmoidDeriv(x float64) float64 {
-	// f(x) = 1/(1+exp(-x)) --> df(x)/dx = exp(-x)/(1+exp(-x)) = f(x)(1-f(x))
-	// So this effectively returns sigmoid(x)*(1-sigmoid(x))
-	e := math.Exp(-x)
-	return e / (1 + e)
+func SigmoidDeriv(x float64) float64 {
+	// f'(x) = f(x)f(-x) = f(x)(1 - f(x))
+	// See sigmoid for properties of f.
+	y := Sigmoid(x)
+	return y * (1 - y)
 }
